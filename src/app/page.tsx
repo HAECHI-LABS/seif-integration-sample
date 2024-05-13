856467ec4e2bfbf418250d3dffb42452220baae9
dot-haechi <dot@haechi.io>
2024-05-13 19:33:21 +0900

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { RainbowKitSample } from "@/app/RainbowKitSample";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start gap-5 p-24">
+      <RainbowKitSample />
+    </main>
+  );
+}
